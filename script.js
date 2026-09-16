6f20838adbbbf0119517bf8bdefa3c145fe78933
K00341147: Levente Alfoldy <K00341147@student.tus.ie>
2026-09-16 14:36:02 +0100

    Add Click Attack JavaScript

---
+++ script.js
@@ -0,0 +1,39 @@
+let score = 0;
+
+const scoreDisplay = document.getElementById("score");
+const title = document.getElementById("title");
+const attackButton = document.getElementById("attackButton");
+const resetButton = document.getElementById("resetButton");
+const powerButton = document.getElementById("powerButton");
+
+function updateDisplay() {
+    scoreDisplay.innerText = score;
+
+    if (score >= 20) {
+        title.innerText = "You win!";
+    }
+}
+
+// TODO: create addPoint()
+function addPoint() {
+    score++;
+    updateDisplay();
+}
+
+// TODO: create addPowerPoint()
+function addPowerPoint() {
+    score += 5;
+    updateDisplay();
+}
+
+// TODO: create resetGame()
+function resetGame() {
+    score = 0;
+    updateDisplay();
+    title.innerText = "Click Attack";
+}
+
+// TODO: connect both functions to buttons
+attackButton.addEventListener("click", addPoint);
+powerButton.addEventListener("click", addPowerPoint);
+resetButton.addEventListener("click", resetGame);
